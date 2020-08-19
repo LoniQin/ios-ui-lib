1aea11dae80983830339c91282412e33da862c2c
LoniQin <longqinlonger@hotmail.com>
2020-08-20 01:21:50 +0800

    Add Router.

---
+++ Sources/UILib/Router.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by lonnie on 2020/8/20.
+//
+
+import Foundation
+public struct Router {
+    
+    public enum Method {
+        case present
+        case push
+    }
+    
+    public let from: UIViewControllerConvertable
+    
+    public let to: UIViewControllerConvertable
+    
+    public let method: Method = .push
+    
+    public let animated: Bool = true
+    
+    public let completion: ()->Void = {}
+    
+    func start() {
+        switch method {
+        case .push:
+            from.toViewController().navigationController?.pushViewController(to.toViewController(), animated: animated)
+            completion()
+        default:
+            from.toViewController().present(to.toViewController(), animated: animated, completion: completion)
+        }
+    }
+    
+}
--- Sources/UILib/UIError.swift
+++ Sources/UILib/UIError.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+
 public enum UIError: Error {
     case imageSourceTypeNotAvailable
 }
