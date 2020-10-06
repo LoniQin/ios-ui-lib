e6435797790215648a3957458216764aa53de103
LoniQin <longqinlonger@hotmail.com>
2020-10-06 18:14:22 +0800

    Update code.

--- Tests/UILibTests/AppBuilderTests.swift
+++ Tests/UILibTests/AppBuilderTests.swift
@@ -11,8 +11,8 @@ final class AppBuilderTests: XCTestCase {
     func testAppBuilder() {
         let app = App(window: nil)
         app.builder.rootViewController(UITabBarController().with(\.viewControllers, Array<ViewController> {
-            ViewController().with(\.handlers, [
-                .init(event: .didLoad) { viewController, _ in
+            ViewController().with(\.handlers, Array {
+                ViewController.Handler(event: .didLoad) { viewController, _ in
                     print("Did load")
                     let label = UILabel().with(\.text, "First")
                     viewController.view.addSubview(label)
@@ -20,25 +20,25 @@ final class AppBuilderTests: XCTestCase {
                     viewController.contentLabel = label
                     let contentLabel: UILabel = viewController.contentLabel
                     contentLabel.text?.assert.equal("First")
-                },
-                .init(event: .willAppear) { viewController ,_ in
+                }
+                ViewController.Handler(event: .willAppear) { viewController ,_ in
                     let label = viewController.contentLabel as? UILabel
                     label?.text.assert.equal("First")
                     
                 }
-            ]).with(\.tabBarItem, UITabBarItem(title: "First", image: nil, selectedImage: nil))
-            ViewController().with(\.handlers, [
-                .init(event: .didLoad) { a, _ in
+            }).with(\.tabBarItem, UITabBarItem(title: "First", image: nil, selectedImage: nil))
+            ViewController().with(\.handlers, Array {
+                ViewController.Handler(event: .didLoad) { a, _ in
                     print("Did load")
                     a.view.backgroundColor = .systemBackground
                     let label = UILabel().with(\.text, "Second")
                     a.view.addSubview(label)
                     label.makeLayout(.equalCenter())
-                },
-                .init(event: .didAppear) { _ ,_ in
+                }
+                ViewController.Handler(event: .didAppear) { _ ,_ in
                     print("Did Appear")
                 }
-            ]).with(\.tabBarItem, UITabBarItem(title: "Second", image: nil, selectedImage: nil))
+            }).with(\.tabBarItem, UITabBarItem(title: "Second", image: nil, selectedImage: nil))
         }))
         let vc = app.window?.rootViewController as? UITabBarController
         vc?.viewControllers?.count.assert.equal(2)
